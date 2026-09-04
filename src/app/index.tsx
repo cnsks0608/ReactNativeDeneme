7578979ee4dc167558fdbe3e7b8c6304f819e7e2
Cansu Köse <cansukose0608@hotmail.com>
2026-09-04 14:56:46 +0300

    First version

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,63 +1,55 @@
-import * as Device from 'expo-device';
-import { Platform, StyleSheet } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { useRef, useState } from 'react';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 
-import { AnimatedIcon } from '@/components/animated-icon';
-import { HintRow } from '@/components/hint-row';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { WebBadge } from '@/components/web-badge';
-import { BottomTabInset, MaxContentWidth, Spacing } from '@/constants/theme';
 
-function getDevMenuHint() {
-  if (Platform.OS === 'web') {
-    return <ThemedText type="small">use browser devtools</ThemedText>;
-  }
-  if (Device.isDevice) {
-    return (
-      <ThemedText type="small">
-        shake device or press <ThemedText type="code">m</ThemedText> in terminal
-      </ThemedText>
-    );
-  }
-  const shortcut = Platform.OS === 'android' ? 'cmd+m (or ctrl+m)' : 'cmd+d';
+function Greeting({
+  name,
+  isWelcomed,
+  onPress }: {
+    name: string;
+    isWelcomed: boolean;
+    onPress: () => void
+  }) {
   return (
-    <ThemedText type="small">
-      press <ThemedText type="code">{shortcut}</ThemedText>
-    </ThemedText>
-  );
+    <Pressable onPress={onPress}>
+      <Text style={styles.greeting}>
+        {isWelcomed ? 'Hoşgeldiniz' : `Merhaba, ${name}!`}
+      </Text>
+    </Pressable>
+  )
+    ;
 }
 
+
+
 export default function HomeScreen() {
+  const [count, setCount] = useState(0);
+  const intervalRef = useRef(0); // ekranın yeniden çizilmesine gerek olmayan ancak hafızada tutulması gereken değişkenler (başlangıçta hiçbir zamanlayıcı çalışmadığı için -> null)
+  const [isWelcomed, setIsWelcomed] = useState(false);
+
+  function baslat(yon:number) {
+    intervalRef.current = // setinterval çalışmaya başlamadan önce verilen bir çeşit kimlik kartı
+      setInterval(() => { // javascriptin hazır fonksiyonu 
+        setCount((count) => count + yon ) 
+      }, 100);  // 100 ms de 1 setcount işini yapar
+  }
+
+  function durdur() {
+    clearInterval(intervalRef.current); // o kimlikli işlemi durdurur 
+  }
+
   return (
-    <ThemedView style={styles.container}>
-      <SafeAreaView style={styles.safeArea}>
-        <ThemedView style={styles.heroSection}>
-          <AnimatedIcon />
-          <ThemedText type="title" style={styles.title}>
-            Welcome to&nbsp;Expo
-          </ThemedText>
-        </ThemedView>
-
-        <ThemedText type="code" style={styles.code}>
-          get started
-        </ThemedText>
-
-        <ThemedView type="backgroundElement" style={styles.stepContainer}>
-          <HintRow
-            title="Try editing"
-            hint={<ThemedText type="code">src/app/index.tsx</ThemedText>}
-          />
-          <HintRow title="Dev tools" hint={getDevMenuHint()} />
-          <HintRow
-            title="Fresh start"
-            hint={<ThemedText type="code">npm run reset-project</ThemedText>}
-          />
-        </ThemedView>
-
-        {Platform.OS === 'web' && <WebBadge />}
-      </SafeAreaView>
-    </ThemedView>
+    <View style={styles.container}>
+      <Greeting name="Cansu" isWelcomed={isWelcomed} onPress={() => isWelcomed ? setIsWelcomed(false) : setIsWelcomed(true)} />
+      <Text style={styles.text}>Sayı: {count}</Text>
+      <Pressable onPressIn={()=> baslat(1)} onPressOut={durdur} >
+        <Text style={{ color: 'green', fontSize: 20 }}>Arttır</Text>
+      </Pressable>
+      <Pressable onPressIn={()=> baslat(-1)} onPressOut={durdur} >
+        <Text style={{ color: 'green', fontSize: 20 }}>Azalt</Text>
+      </Pressable>
+      
+    </View>
   );
 }
 
@@ -65,34 +57,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    flexDirection: 'row',
-  },
-  safeArea: {
-    flex: 1,
-    paddingHorizontal: Spacing.four,
     alignItems: 'center',
-    gap: Spacing.three,
-    paddingBottom: BottomTabInset + Spacing.three,
-    maxWidth: MaxContentWidth,
+    gap: 20,
   },
-  heroSection: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    flex: 1,
-    paddingHorizontal: Spacing.four,
-    gap: Spacing.four,
+  text: {
+    fontSize: 24,
   },
-  title: {
-    textAlign: 'center',
-  },
-  code: {
-    textTransform: 'uppercase',
-  },
-  stepContainer: {
-    gap: Spacing.three,
-    alignSelf: 'stretch',
-    paddingHorizontal: Spacing.three,
-    paddingVertical: Spacing.four,
-    borderRadius: Spacing.four,
-  },
+  greeting: {
+    color: "purple",
+    fontSize: 40,
+    marginBottom: 20,
+  }
 });
